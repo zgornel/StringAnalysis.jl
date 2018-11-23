89eb8bd6e9c126f79c1bb885bfb2955a418a3243
Corneliu Cofaru <cornel@oxoaresearch.com>
2018-11-23 17:02:21 +0100

    Work on the tokenizer

--- src/tokenizer.jl
+++ src/tokenizer.jl
@@ -1,19 +1,34 @@
-# Split string into tokens on whitespace
-tokenize(s::T) where T<:AbstractString = WordTokenizers.tokenize(s)
-tokenize(lang::S, s::T) where {S<:Language, T<:AbstractString} = WordTokenizers.tokenize(s)
+"""
+    sentence_tokenize([lang,] s)
 
-sentence_tokenize(s::T) where T<:AbstractString = WordTokenizers.split_sentences(s)
-sentence_tokenize(lang::S, s::T) where {S<:Language, T<:AbstractString} = WordTokenizers.split_sentences(s)
-
+Splits string `s` into sentences using `WordTokenizers.split_sentences`
+function to perform the tokenization. If a language `lang` is provided,
+it ignores it ;)
+"""
+sentence_tokenize(s::T) where T<:AbstractString =
+    WordTokenizers.split_sentences(s)
+sentence_tokenize(lang::S, s::T) where {S<:Language, T<:AbstractString} =
+    WordTokenizers.split_sentences(s)
 
 
 """
-    tokenize_fast(text [;splitter])
+    tokenize_slow([lang,] s)
 
-Function that quickly tokenizes the `text` based on the splitting
-pattern specified by `splitter::RegEx`. Supported types for`text`
-are: `AbstractString`, `Vector{AbstractString}`, `StringDocument`
-and `NGramDocument`.
+Splits string `s` into tokens on whitespace using `WordTokenizers.tokenize`
+function to perform the tokenization. If a language `lang` is provided,
+it ignores it ;)
+"""
+tokenize_slow(s::T) where T<:AbstractString = WordTokenizers.tokenize(s)
+tokenize_slow(lang::S, s::T) where {S<:Language, T<:AbstractString} = WordTokenizers.tokenize(s)
+
+
+"""
+    tokenize_fast(doc [;splitter])
+
+Function that quickly tokenizes `doc` based on the splitting
+pattern specified by `splitter::RegEx`.
+Supported types for `doc` are: `AbstractString`, `Vector{AbstractString}`,
+`StringDocument` and `NGramDocument`.
 """
 tokenize_fast(doc::Vector{S}; splitter::Regex=DEFAULT_TOKENIZATION_REGEX
              ) where S<:AbstractString =
@@ -22,7 +37,7 @@ tokenize_fast(doc::Vector{S}; splitter::Regex=DEFAULT_TOKENIZATION_REGEX
 tokenize_fast(doc::S; splitter::Regex=DEFAULT_TOKENIZATION_REGEX
              ) where S<:AbstractString = begin
     # First, split
-    tokens = String.(strip.(split(doc, splitter)))
+    tokens = strip.(split(doc, splitter))
     # Filter out empty strings
     return filter!(!isempty, tokens)
 end
@@ -32,3 +47,17 @@ tokenize_fast(doc::NGramDocument; splitter::Regex=DEFAULT_TOKENIZATION_REGEX) =
 
 tokenize_fast(doc::StringDocument; splitter::Regex=DEFAULT_TOKENIZATION_REGEX) =
     tokenize_fast(doc.text, splitter=splitter)
+
+"""
+    tokenize(s [;method])
+
+Tokenizes based on either the `tokenize_slow` or `tokenize_fast`
+functions.
+"""
+function tokenize(doc; method::String="slow")
+    if method == "fast"
+        return tokenize_fast(doc)
+    else
+        return tokenize_slow(doc)
+    end
+end
